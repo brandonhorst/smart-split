f349774b1915ef0a347f09bf3a9b90ea8afaa1a2
Brandon Horst <brandonhorst@gmail.com>
2015-03-26 14:19:02 -0400

    Add standard

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-module.exports = function split(input, strOrRegex) {
+module.exports = function split (input, strOrRegex) {
   var results = []
 
   if (strOrRegex instanceof RegExp) {
     var regex = new RegExp(strOrRegex.toString().slice(1, -1), 'g')
     var oldIndex = 0
     var match
-    while (match = regex.exec(input)) {
+    while ((match = regex.exec(input))) {
       results.push(input.substring(oldIndex, match.index))
       results.push(match[0])
       oldIndex = match.index + match[0].length
@@ -15,7 +15,7 @@ module.exports = function split(input, strOrRegex) {
     var splits = input.split(strOrRegex)
     var item
     for (var i = 0, l = splits.length; i < l - 1; i++) {
-      var item = splits[i]
+      item = splits[i]
       results.push(item)
       results.push(strOrRegex)
     }
